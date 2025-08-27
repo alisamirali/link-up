c2747ec971228560b715eab76f26ecc848f33f52
Ali Samir <ali.samir@taager.com>
2025-08-27 10:04:31 +0300

    feat: add member update and removal functionalities with corresponding hooks and UI integration

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
-import { Id } from "./_generated/dataModel";
-import { query, QueryCtx } from "./_generated/server";
+import { Doc, Id } from "./_generated/dataModel";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { auth } from "./auth";
 
 const populateUser = (ctx: QueryCtx, userId: Id<"users">) => {
@@ -30,7 +30,7 @@ export const get = query({
       )
       .collect();
 
-    const members = [];
+    const members: Array<Doc<"members"> & { user: Doc<"users"> }> = [];
 
     for (const member of data) {
       const user = await populateUser(ctx, member.userId);
@@ -91,5 +91,101 @@ export const current = query({
     if (!member) return null;
 
     return member;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("members"),
+    role: v.union(v.literal("admin"), v.literal("member")),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+    if (!userId) throw new Error("Unauthorized");
+
+    const member = await ctx.db.get(args.id);
+    if (!member) throw new Error("Member not found");
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!currentMember || currentMember.role !== "admin")
+      throw new Error("Unauthorized");
+
+    await ctx.db.patch(args.id, {
+      role: args.role,
+    });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("members"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+    if (!userId) throw new Error("Unauthorized");
+
+    const member = await ctx.db.get(args.id);
+    if (!member) throw new Error("Member not found");
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!currentMember) throw new Error("Unauthorized");
+
+    if (member.role === "admin") {
+      throw new Error("Cannot remove admin member");
+    }
+
+    if (currentMember._id === args.id && currentMember.role === "admin") {
+      throw new Error("Cannot remove self if self is an admin");
+    }
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("reactions")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("conversations")
+        .filter((q) =>
+          q.or(
+            q.eq(q.field("memberOneId"), member._id),
+            q.eq(q.field("memberTwoId"), member._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
   },
 });
--- src/features/members/api/index.ts
+++ src/features/members/api/index.ts
@@ -1,3 +1,5 @@
 export * from "./use-current-member";
 export * from "./use-get-member";
 export * from "./use-get-members";
+export * from "./use-remove-member";
+export * from "./use-update-member";
